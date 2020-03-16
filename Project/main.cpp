d8066506dd14e784f3db11b041f4012309cf54d1
John Green <jkgreen@uclan.ac.uk>
2020-03-16 13:55:55 +0000

    Added base project

---
+++ Project/main.cpp
@@ -0,0 +1,28 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <memory>
+
+#define THREE_D 1
+
+typedef unsigned int uint32_t;
+
+
+const uint32_t sphere_count = 100;
+
+
+int main(int argc, char **argv)
+{
+	float X[sphere_count]{ 0.0f };
+	float Y[sphere_count]{ 0.0f };
+#if THREE_D
+	float Z[sphere_count]{ 0.0f };
+#endif
+
+
+
+
+
+
+	return 0;
+}
